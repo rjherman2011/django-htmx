fd7c89203354e736b0d8376a71a4710cef2679a8
Bob Herman <rjherman2011@gmail.com>
2023-09-06 15:52:08 -0500

    adding in some basic data dirs

--- app/main.py
+++ app/main.py
@@ -1,15 +1,39 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+import subprocess
+from contextlib import asynccontextmanager
+
 from app.config import Settings
 from app.routes import router
 
 settings = Settings()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Context manager for FastAPI app. It will run all code before `yield`
+    on app startup, and will run code after `yeld` on app shutdown.
+    """
+
+    try:
+        subprocess.run([
+            "tailwindcss",
+            "-i",
+            f"{settings.STATIC_DIR}/src/tw.css",
+            "-o",
+            f"{settings.STATIC_DIR}/main.css",
+            #TODO - add "--minify" in prod versions
+        ])
+
+    except Exception as e:
+        print(f"Error running tailwindcss: {e}")
+
+    yield
+
 def get_app() -> FastAPI:
     """Create a FastAPI app with the specified settings."""
 
-    app = FastAPI(**settings.fastapi_kwargs)
+    app = FastAPI(**settings.fastapi_kwargs, lifespan=lifespan)
 
     app.include_router(router)
 
