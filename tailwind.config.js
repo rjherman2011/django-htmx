29e3ece43c15f4a699272a2451d55a9c22e988bc
Bob Herman <rjherman2011@gmail.com>
2023-09-06 15:33:32 -0500

    adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/templates/**/*.{html, jinja}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
