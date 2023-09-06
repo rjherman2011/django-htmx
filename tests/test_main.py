cef413a0abc32e5186034b1113fcb148f5a5068e
Bob Herman <rjherman2011@gmail.com>
2023-09-06 16:18:04 -0500

    fixing broken tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,4 +2,4 @@
 def test_read_main(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert "<h1>Hello World!</h1>" in response.text
+    assert """<h2 class="text-2xl leading-relaxed text-slate-800 uppercase">Welcome</h2>""" in response.text
